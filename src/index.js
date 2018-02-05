a9d6d7963b095a902003d07f6f7f9b5fd92d9902
Denys <dartist21@gmail.com>
2018-02-05 23:20:36 +0200

    finish lesson 3

--- src/index.js
+++ src/index.js
@@ -1,38 +1,42 @@
+import _ from "lodash";
+import moment from "moment";
+import { markdown } from "markdown";
+
 // 1. Fetch the new data
-fetch('/data')
-    .then(res => res.json())
-    .then(data => {
-        renderData(data);
-    });
+fetch("/data")
+  .then(res => res.json())
+  .then(data => {
+    renderData(data);
+  });
 
 // 2. Setup adding new posts
-const addPostForm = document.querySelector('#add-post');
-addPostForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const content = addPostForm.querySelector('.add-post__textarea').value;
-    addNewRecord(window.markdown.toHTML(content));
+const addPostForm = document.querySelector("#add-post");
+addPostForm.addEventListener("submit", event => {
+  event.preventDefault();
+  const content = addPostForm.querySelector(".add-post__textarea").value;
+  addNewRecord(markdown.toHTML(content));
 });
 
 // 3. Methods
-const renderData = (data) => {
-    const target = document.querySelector('#records');
+const renderData = data => {
+  const target = document.querySelector("#records");
 
-    const html = window._(data.items)
-        .map(item => generateRecord(item.date, window.markdown.toHTML(item.content)))
-        .join('');
+  const html = _(data.items)
+    .map(item => generateRecord(item.date, markdown.toHTML(item.content)))
+    .join("");
 
-    target.innerHTML += html;
+  target.innerHTML += html;
 };
 
-const addNewRecord = (content) => {
-    const target = document.querySelector('#records');
-    const html = generateRecord(new Date(), content);
-    target.innerHTML = html + target.innerHTML;
+const addNewRecord = content => {
+  const target = document.querySelector("#records");
+  const html = generateRecord(new Date(), content);
+  target.innerHTML = html + target.innerHTML;
 };
 
 const generateRecord = (date, content) => {
-    return `<section class="record">
-        <div class="record__date">${window.moment(date).format('D MMM YYYY')}</div>
+  return `<section class="record">
+        <div class="record__date">${moment(date).format("D MMM YYYY")}</div>
         <div class="record__content">${content}</div>
     </section>`;
-}
+};
