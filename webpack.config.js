d431a78578f45ac01321d0a252bf5c3f26f93260
Denys <dartist21@gmail.com>
2018-02-08 09:45:27 +0200

    feat: add css-hot-loader

--- webpack.config.js
+++ webpack.config.js
@@ -68,6 +68,10 @@ if (isProduction) {
     }),
   ]);
 
+  config.module.rules[0].use = ["css-hot-loader"].concat(
+    config.module.rules[0].use
+  );
+
   config.devServer = {
     contentBase: path.resolve(__dirname, "public"),
     publicPath: "/feed/",
