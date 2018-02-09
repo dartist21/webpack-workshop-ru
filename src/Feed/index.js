efafd04d393288bad78a707d96cb0542d25263e4
Denys <dartist21@gmail.com>
2018-02-09 23:35:51 +0200

    lesson 9: lazy loading pages in webpack with import()

--- src/Feed/index.js
+++ src/Feed/index.js
@@ -1,17 +1,16 @@
-import {render as renderMarkdownFeed} from '../FeedMarkdown/';
-import {render as renderRawFeed} from '../FeedRaw/';
-
 const render = () => {
-    const hash = document.location.hash;
-    const renderByHash = {
-        '#markdown': renderMarkdownFeed,
-        '#raw': renderRawFeed,
-    };
+  const hash = document.location.hash;
+  const renderByHash = {
+    "#markdown": () => import("../FeedMarkdown/"),
+    "#raw": () => import("../FeedRaw/"),
+  };
 
-    if (renderByHash[hash]) {
-        renderByHash[hash]();
-    }
+  if (renderByHash[hash]) {
+    renderByHash[hash]().then(module => {
+      module.render();
+    });
+  }
 };
 
-window.addEventListener('hashchange', render);
+window.addEventListener("hashchange", render);
 render();
--- webpack.config.js
+++ webpack.config.js
@@ -1,78 +1,84 @@
-const path = require('path');
-const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-const isProduction = process.env.NODE_ENV === 'production';
+const isProduction = process.env.NODE_ENV === "production";
 
 const basicConfig = {
-    entry: {
-        home: './src/Home/index.js',
-        feed: './src/Feed/index.js'
-    },
-    output: {
-        path: path.resolve(__dirname, 'public', 'assets'),
-        filename: '[name].js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: {
-                        loader: 'css-loader',
-                        options: {
-                            minimize: true
-                        }
-                    }
-                })
+  entry: {
+    home: "./src/Home/index.js",
+    feed: "./src/Feed/index.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "public", "assets"),
+    filename: "[name].js",
+    publicPath: "/assets/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: {
+            loader: "css-loader",
+            options: {
+              minimize: true,
             },
-            {
-                test: /\.js/,
-                use: [
-                    {
-                        loader: 'babel-loader',
-                        options: {
-                            presets: [['env', {
-                                "targets": {
-                                  "browsers": ["last 2 versions", "ie >= 11"]
-                                }
-                            }]],
-                            plugins: ['lodash']
-                        }
-                    }
-                ]
-            }
+          },
+        }),
+      },
+      {
+        test: /\.js/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                [
+                  "env",
+                  {
+                    targets: {
+                      browsers: ["last 2 versions", "ie >= 11"],
+                    },
+                  },
+                ],
+              ],
+              plugins: ["lodash", "syntax-dynamic-import"],
+            },
+          },
         ],
-    },
-    plugins: [
-        new ExtractTextPlugin('feed.css')
+      },
     ],
+  },
+  plugins: [new ExtractTextPlugin("feed.css")],
 };
 
 if (isProduction) {
-    basicConfig.plugins = basicConfig.plugins.concat([
-        new webpack.optimize.UglifyJsPlugin(),
-        new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-        new webpack.DefinePlugin({
-            'process.env.NODE_ENV': '"production"',
-        }),
-        new webpack.optimize.CommonsChunkPlugin({
-            name: 'common'
-        })
-    ]);
+  basicConfig.plugins = basicConfig.plugins.concat([
+    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+    new webpack.DefinePlugin({
+      "process.env.NODE_ENV": '"production"',
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: "common",
+    }),
+  ]);
 } else {
-    basicConfig.devServer = {
-        contentBase: path.join(__dirname, "public"),
-        publicPath: '/assets/',
-        compress: true,
-        port: 9000,
-        proxy: {
-            "/data": "http://localhost:3000"
-        },
-    };
+  basicConfig.devServer = {
+    contentBase: path.join(__dirname, "public"),
+    publicPath: "/assets/",
+    compress: true,
+    port: 9000,
+    proxy: {
+      "/data": "http://localhost:3000",
+    },
+  };
 
-    basicConfig.module.rules[0].use = ['css-hot-loader'].concat(basicConfig.module.rules[0].use);
+  basicConfig.module.rules[0].use = ["css-hot-loader"].concat(
+    basicConfig.module.rules[0].use
+  );
 }
 
 module.exports = basicConfig;
